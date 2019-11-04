52492412ce6ca860559168501ee11b28c453a6a9
Juliette Sinibardy <juju.siny@gmail.com>
2019-11-04 09:18:01 +0100

    :lipstick: Add Bootstrap & basic layout

--- deep_sea.rb
+++ deep_sea.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
+require 'securerandom'
+
+configure do
+  enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+end
 
 get '/' do
-  "Arrhhh piratzzz"
+  erb :home
 end
