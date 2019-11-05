2767ec5f93eb7cc677a9afd50e90552e9e839b7b
Juliette Sinibardy <juju.siny@gmail.com>
2019-11-05 10:58:37 +0100

    :ok_hand: Add small details

--- player.rb
+++ player.rb
@@ -2,13 +2,17 @@ class Player
   attr_accessor :going_up
 
   def initialize(name)
-    @name      = name
-    @score     = 0
+    @name  = name
+    @score = 0
+    reset
+  end
+
+  def reset
     @treasures = 0
     @going_up  = false
     @is_back   = false
   end
-
+  
   def to_s
     @name
   end
